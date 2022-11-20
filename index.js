1514d27c9fa8b6a2110d32212bc0526b33aeb53d
Fulbabu <fulbabucse@gmail.com>
2022-11-20 14:11:41 +0600

    stripe payment indent

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -78,6 +79,21 @@ const dbConnect = async () => {
       }
       next();
     };
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const amount = parseInt(booking.price) * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
     app.get("/doctors", JWTVerify, verifyAdmin, async (req, res) => {
       const query = {};
